4731865527432fc257ab5814348017207bd612f2
daniel-henao <danior-0208@hotmail.com>
2023-10-25 22:50:53 -0500

    Primera-entrega-con-clases

---
+++ src/classes/productManager.js
@@ -0,0 +1,104 @@
+import fs from "fs/promises";
+
+class ProductManager {
+  constructor(productsFile) {
+    this.productsFile = productsFile;
+  }
+
+  async initialize() {
+    try {
+      await fs.access(this.productsFile);
+    } catch (error) {
+      await fs.writeFile(this.productsFile, "[]", "utf8");
+    }
+  }
+
+  async getProducts(limit) {
+    const data = await fs.readFile(this.productsFile, "utf8");
+    const products = JSON.parse(data);
+
+    if (limit) {
+      return products.slice(0, parseInt(limit, 10));
+    } else {
+      return products;
+    }
+  }
+
+  async getProductById(id) {
+    const data = await fs.readFile(this.productsFile, "utf8");
+    const products = JSON.parse(data);
+
+    const product = products.find((p) => p.id == id);
+
+    if (product) {
+      return product;
+    } else {
+      throw new Error("Producto no encontrado");
+    }
+  }
+
+  async addProduct(newProductData) {
+    const data = await fs.readFile(this.productsFile, "utf8");
+    const products = JSON.parse(data);
+
+    const newProductId = products.length + 1;
+    const newProduct = {
+      id: newProductId,
+      ...newProductData,
+    };
+
+    products.push(newProduct);
+
+    await fs.writeFile(
+      this.productsFile,
+      JSON.stringify(products, null, 2),
+      "utf8"
+    );
+
+    return newProduct;
+  }
+
+  async updateProduct(id, updatedProductData) {
+    const data = await fs.readFile(this.productsFile, "utf8");
+    const products = JSON.parse(data);
+
+    const productIndex = products.findIndex((p) => p.id == id);
+
+    if (productIndex === -1) {
+      throw new Error("Producto no encontrado");
+    }
+
+    updatedProductData.id = id;
+
+    products[productIndex] = updatedProductData;
+
+    await fs.writeFile(
+      this.productsFile,
+      JSON.stringify(products, null, 2),
+      "utf8"
+    );
+
+    return updatedProductData;
+  }
+
+  async deleteProduct(id) {
+    const data = await fs.readFile(this.productsFile, "utf8");
+    const products = JSON.parse(data);
+
+    const productIndex = products.findIndex((p) => p.id == id);
+
+    if (productIndex === -1) {
+      throw new Error("Producto no encontrado");
+    }
+
+    products.splice(productIndex, 1);
+
+    await fs.writeFile(
+      this.productsFile,
+      JSON.stringify(products, null, 2),
+      "utf8"
+    );
+  }
+}
+
+export default ProductManager;
